4d91707f5111f4970408dddb6434e5a31d99290c
Maxim Kraychev <theneraxes@gmail.com>
2022-06-19 15:33:00 +0300

    adding new folder

---
+++ Fundamentals/01DataTypesAndVariablesLab/echoType.js
@@ -0,0 +1,14 @@
+function echoType(input) {
+
+    let type = typeof input
+
+    console.log(type);
+
+    if(type === "string" || type === "number") {
+        console.log(input);
+    } else {
+        console.log('Parameter is not suitable for printing');
+    }
+}
+
+echoType(null)
