37b85800770ffc8b50ef7337b8a23365cbe3ab1d
Maxim Kraychev <theneraxes@gmail.com>
2022-09-26 22:08:58 +0300

    Formatted text.

--- Fundamentals/01DataTypesAndVariablesLab/triplesOfLatinLetters.js
+++ Fundamentals/01DataTypesAndVariablesLab/triplesOfLatinLetters.js
@@ -1,21 +1,21 @@
-function triplesOfLatinLetters (numberAsString) { 
+function triplesOfLatinLetters(numberAsString) {
 
     let inputAsNumber = Number(numberAsString);
-    
 
-    for(let i = 0; i < inputAsNumber; i++) {
-        let firstLetter = String.fromCharCode(97+i);
-        
-        for(let j = 0; j < inputAsNumber; j++) {
-           let secondLetter = firstLetter + String.fromCharCode(97+j);
-            
-            for(let k = 0; k < inputAsNumber; k++) {
-                let lastLetter = secondLetter + String.fromCharCode(97+k);
+
+    for (let i = 0; i < inputAsNumber; i++) {
+        let firstLetter = String.fromCharCode(97 + i);
+
+        for (let j = 0; j < inputAsNumber; j++) {
+            let secondLetter = firstLetter + String.fromCharCode(97 + j);
+
+            for (let k = 0; k < inputAsNumber; k++) {
+                let lastLetter = secondLetter + String.fromCharCode(97 + k);
                 console.log(lastLetter);
                 lastLetter = '';
             }
         }
-        
+
     }
 
 }
