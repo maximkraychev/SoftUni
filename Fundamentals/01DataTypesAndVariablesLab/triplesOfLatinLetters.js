09c618febee1834a710387d231d22ebd1c16091a
Maxim Kraychev <theneraxes@gmail.com>
2022-09-26 22:04:27 +0300

    Added exercise 10 - Triples of Latin Letters

---
+++ Fundamentals/01DataTypesAndVariablesLab/triplesOfLatinLetters.js
@@ -0,0 +1,23 @@
+function triplesOfLatinLetters (numberAsString) { 
+
+    let inputAsNumber = Number(numberAsString);
+    
+
+    for(let i = 0; i < inputAsNumber; i++) {
+        let firstLetter = String.fromCharCode(97+i);
+        
+        for(let j = 0; j < inputAsNumber; j++) {
+           let secondLetter = firstLetter + String.fromCharCode(97+j);
+            
+            for(let k = 0; k < inputAsNumber; k++) {
+                let lastLetter = secondLetter + String.fromCharCode(97+k);
+                console.log(lastLetter);
+                lastLetter = '';
+            }
+        }
+        
+    }
+
+}
+
+triplesOfLatinLetters("3"); 
