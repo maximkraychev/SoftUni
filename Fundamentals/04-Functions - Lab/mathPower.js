1e43907eb3b46dbdb67ba7e5f6edb3f76954e452
Maxim Kraychev <theneraxes@gmail.com>
2022-10-07 22:07:41 +0300

    Added exercise:    02. Math Power

---
+++ Fundamentals/04-Functions - Lab/mathPower.js
@@ -0,0 +1,11 @@
+function mathPower(inputNumber, inputPower) {
+
+    console.log(numberToThePowerOf(inputNumber, inputPower));
+
+    function numberToThePowerOf(number, power) {
+        return (number ** power);
+    }
+
+}
+
+mathPower(2,8);
