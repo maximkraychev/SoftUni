1228f8f1b9b6b08f02b60a7761fa2ab6b07e50ae
Maxim Kraychev <theneraxes@gmail.com>
2022-11-02 13:54:15 +0200

    Updated exercise: 03. School Register

--- Fundamentals/06-Objects and Classes - More Exercises/03-schoolRegister.js
+++ Fundamentals/06-Objects and Classes - More Exercises/03-schoolRegister.js
@@ -22,7 +22,9 @@ function schoolRegister(listWithInformation) {
         }
     })
 
-    for (grade in gradesWithAllStudends) {
+    let sortedGrades = Object.keys(gradesWithAllStudends).sort((a, b) => a - b);
+
+    for (grade of sortedGrades) {
         let score = 0;
         let names = [];
         let countOfStudents = 0
@@ -37,29 +39,29 @@ function schoolRegister(listWithInformation) {
     }
 }
 
-// schoolRegister([
-//         "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
-//         "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
-//         "Student name: George, Grade: 8, Graduated with an average score: 2.83",
-//         "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
-//         "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
-//         "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
-//         "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
-//         "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
-//         "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
-//         "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
-//         "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
-//         "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"
-//     ]
-//     );
-
 schoolRegister([
-    'Student name: George, Grade: 5, Graduated with an average score: 2.75',
-    'Student name: Alex, Grade: 9, Graduated with an average score: 3.66',
-    'Student name: Peter, Grade: 8, Graduated with an average score: 2.83',
-    'Student name: Boby, Grade: 5, Graduated with an average score: 4.20',
-    'Student name: John, Grade: 9, Graduated with an average score: 2.90',
-    'Student name: Steven, Grade: 2, Graduated with an average score: 4.90',
-    'Student name: Darsy, Grade: 1, Graduated with an average score: 5.15'
-]
-)
+        "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
+        "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
+        "Student name: George, Grade: 8, Graduated with an average score: 2.83",
+        "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
+        "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
+        "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
+        "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
+        "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
+        "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
+        "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
+        "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
+        "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"
+    ]
+    );
+
+// schoolRegister([
+//     'Student name: George, Grade: 5, Graduated with an average score: 2.75',
+//     'Student name: Alex, Grade: 9, Graduated with an average score: 3.66',
+//     'Student name: Peter, Grade: 8, Graduated with an average score: 2.83',
+//     'Student name: Boby, Grade: 5, Graduated with an average score: 4.20',
+//     'Student name: John, Grade: 9, Graduated with an average score: 2.90',
+//     'Student name: Steven, Grade: 2, Graduated with an average score: 4.90',
+//     'Student name: Darsy, Grade: 1, Graduated with an average score: 5.15'
+// ]
+// )
