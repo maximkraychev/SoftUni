3edf36c314591c5c8d718808a7dd171b955bac35
Maxim Kraychev <theneraxes@gmail.com>
2022-12-01 23:53:28 +0200

    Adding exercise: 06. Extract Emails > with updated regExp 100/100 in Judge

--- Fundamentals/09-Regular Expressions - Exercise/06-extractEmails.js
+++ Fundamentals/09-Regular Expressions - Exercise/06-extractEmails.js
@@ -1,15 +1,17 @@
 function extractEmails(text) {
 
     text = text.toString();
-    const regExp = /(\b|_)(?<email>[^_\.-][a-zA-Z\d\.\_\-]+[^_]\b@\b[a-zA-Z\.\-]+\b\.\b[a-zA-Z\.\-]+\b)/g;
+    const regExp = /(?<!\S)(?<user>[a-z\d]+((?<test>[\.\-\_]*)[a-z\d]+)?(\k<test>[a-z\d]+)*)@(?<host>[a-z]+([\-]?[a-z])*([\.][a-z]+([\-]?[a-z])*)+)/g;
 
     const matches = text.matchAll(regExp);
 
     if (matches !== null) {
         for (const match of matches) {
-            console.log(match.groups.email);
+            console.log(`${match.groups.user}@${match.groups.host}`);
         }
     }
 }
 
 extractEmails('Please contact us at: support@github.com.');
+extractEmails('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.')
+extractEmails('Many users @ SoftUni confuse email addresses. We @ Softuni.BG provide high-quality training @ home or @ class. –- steve.parker@softuni.de.')
