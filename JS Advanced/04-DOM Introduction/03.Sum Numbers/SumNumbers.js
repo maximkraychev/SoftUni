4700f97f6346e2a6688c643e0b10a3c85682f852
Maxim Kraychev <theneraxes@gmail.com>
2022-12-29 18:28:08 +0200

    Adding exercise 03. Sum Numbers

---
+++ JS Advanced/04-DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+    let num1 = document.getElementById('num1');
+    let num2 = document.getElementById('num2');
+    let sum = document.getElementById('sum');
+
+    sum.value = Number(num1.value) + Number(num2.value);
+}
