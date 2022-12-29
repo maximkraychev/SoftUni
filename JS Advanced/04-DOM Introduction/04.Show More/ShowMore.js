2bd5888ddcda2d77399de0bd403fbb8e92318331
Maxim Kraychev <theneraxes@gmail.com>
2022-12-29 18:40:20 +0200

    Adding exercise: 04. Show More

---
+++ JS Advanced/04-DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let aElement = document.getElementById('more');
+    let spanElement = document.getElementById('text');
+
+    aElement.style.display = 'none';
+    spanElement.style.display = 'inline';
+}
