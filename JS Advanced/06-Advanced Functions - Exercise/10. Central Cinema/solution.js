3cfd73268e02caf654c40a7cd33212f2efa67b14
Maxim Kraychev <theneraxes@gmail.com>
2023-01-17 12:00:50 +0200

    Adding exercise: 10. Central Cinema

--- JS Advanced/06-Advanced Functions - Exercise/10. Central Cinema/solution.js
+++ JS Advanced/06-Advanced Functions - Exercise/10. Central Cinema/solution.js
@@ -6,30 +6,61 @@ function solve() {
     const buttonClear = document.getElementById('archive').querySelector('button');
 
     onScreenButton.addEventListener('click', onScreen);
+    buttonClear.addEventListener('click', onClear);
 
     function onScreen(e) {
         e.preventDefault()
-        if (movieName.value !== '' && hall.value !== '' && ) { // TODO...
-
-            console.log(movieName.value == '');
-            console.log(hall.value == '');
-            console.log(Number(ticketPrice.value) !== NaN);
-
+        if (movieName.value !== '' && hall.value !== '' && ticketPrice.value.match(/\b\d+\b/g)) { // TODO...
 
             const listElement = elementCreator('li');
             listElement.appendChild(elementCreator('span', movieName.value));
-            listElement.appendChild(elementCreator('strong', hall.value));
+            listElement.appendChild(elementCreator('strong', `Hall: ${hall.value}`));
 
             const divElement = elementCreator('div');
             divElement.appendChild(elementCreator('strong', ticketPrice.value))
             divElement.appendChild(elementCreator('input', '', 'Tickets Sold'));
 
             const archiveButton = elementCreator('button', 'Archive');
+            archiveButton.addEventListener('click', onArhive);
             divElement.appendChild(archiveButton);
             listElement.appendChild(divElement);
             listMoviesElement.appendChild(listElement);
+
+            movieName.value = '';
+            hall.value = '';
+            ticketPrice.value = '';
         }
     }
+
+    function onArhive(e) {
+        const movieListElement = e.target.parentNode.parentNode;
+        const inputTicketsSoldElement = e.target.previousSibling;
+        const movieTicketPrice = inputTicketsSoldElement.previousSibling.textContent;
+        const totalAmount = movieListElement.querySelector('strong');
+        const divElement = movieListElement.querySelector('div');
+
+        if (inputTicketsSoldElement.value.match(/\b\d+\b/g)) {
+
+            const sum = Number(inputTicketsSoldElement.value) * Number(movieTicketPrice);
+            totalAmount.textContent = `Total amount: ${sum.toFixed(2)}`;
+            divElement.remove();
+
+            const deleteButton = elementCreator('button', 'Delete')
+            deleteButton.addEventListener('click', () => {
+                movieListElement.remove();
+            });
+
+            movieListElement.appendChild(deleteButton);
+            listArchiveElement.appendChild(movieListElement);
+        }
+    }
+
+    function onClear() {
+        while (listArchiveElement.firstChild) {
+            listArchiveElement.firstChild.remove()
+        }
+    }
+
 
     function elementCreator(element, text, placeholderText) {
         const newElement = document.createElement(element);
