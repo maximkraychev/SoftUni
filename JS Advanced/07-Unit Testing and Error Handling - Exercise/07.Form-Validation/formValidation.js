a465e54f3959d51a98ebf99ac5b7ce3a33bbfcdf
Maxim Kraychev <theneraxes@gmail.com>
2023-02-04 23:48:56 +0200

    Exercise 07. Form Validation   //TO DO... 88/100 in Judge

--- JS Advanced/07-Unit Testing and Error Handling - Exercise/07.Form-Validation/formValidation.js
+++ JS Advanced/07-Unit Testing and Error Handling - Exercise/07.Form-Validation/formValidation.js
@@ -5,68 +5,62 @@ function validate() {
 
     const objWithRegex = {
         username: /^[a-zA-z0-9]{3,20}$/g,
-        email: /.*@\.+.*/g,
+        email: /^.*@.*\..*$/g,
         password: /^[\w]{5,15}$/g,
         companyNumber: /^[0-9]{4}$/g,
     }
-    const alltypedInputs = Array.from(document.querySelectorAll('input:not([type=checkbox])'));
-    const buttonSubmit = document.getElementById('submit');
 
+    const buttonSubmit = document.getElementById('submit');
     buttonSubmit.addEventListener('click', check);
 
     function check(event) {
         event.preventDefault();
+        let validInput = [];
+        let invalidInput = [];
 
-        let validInput = {};
-        let invalidInput = {};
+        const usernameElement = document.querySelector('#username');
+        if (usernameElement.value.match(objWithRegex.username)) {
+            validInput.push(usernameElement);
+        } else {
+            invalidInput.push(usernameElement);
+        }
 
-        alltypedInputs.forEach(element => {
-            const elementID = element['id'];
-            const elementText = element.value;
+        const emailElement = document.querySelector('#email');
+        if (emailElement.value.match(objWithRegex.email)) {
+            validInput.push(emailElement);
+        } else {
+            invalidInput.push(emailElement);
+        }
 
-            if (elementID == 'confirm-password') {
-                if (validInput.hasOwnProperty('password') && validInput.password.value === elementText) {
-                    validInput[elementID] = element;
-                    element.style.border = 'none';
-                } else {
-                    invalidInput[elementID] = element;
-                    element.style.borderColor = 'red';
-                }
-                return;
-            }
+        const passwordElement = document.querySelector('#password');
+        const confirmPasswordElement = document.querySelector('#confirm-password');
+        if (passwordElement.value.match(objWithRegex.password) && passwordElement.value === confirmPasswordElement.value) {
+            validInput.push(passwordElement);
+            validInput.push(passwordElement);
+        } else {
+            invalidInput.push(passwordElement);
+            invalidInput.push(confirmPasswordElement);
+        }
 
-            if (elementID == 'companyNumber') {
-                if (checkBoxElement.checked) {
-                    if (elementText.match(objWithRegex[elementID])) {
-                        validInput[elementID] = element;
-                        element.style.border = 'none';
-                    } else {
-                        invalidInput[elementID] = element;
-                        element.style.borderColor = 'red';
-                    }
-                }
-                return;
-            }
-
-            if (elementText.match(objWithRegex[elementID])) {
-                validInput[elementID] = element;
-                element.style.border = 'none';
+        if (checkBoxElement.checked) {
+            const companyElement = document.querySelector('#companyNumber');
+            if (companyElement.value.match(objWithRegex.companyNumber)) {
+                validInput.push(companyElement);
             } else {
-                invalidInput[elementID] = element;
-                element.style.borderColor = 'red';
+                invalidInput.push(companyElement);
             }
-        })
-        console.log(invalidInput);
+        }
+
+        validInput.forEach(el => el.style = 'border: none');
+        invalidInput.forEach(el => el.style = 'border-color: red');
 
         const divToShowHide = document.getElementById('valid');
-        if (Object.keys(invalidInput).length == 0) {
+        if (invalidInput.length == 0) {
             divToShowHide.style.display = 'block';
         } else {
             divToShowHide.style.display = 'none';
         }
-
     }
-
 
     function showHideCompanyNumber() {
         const companyFielDsetElement = document.getElementById('companyInfo');
@@ -77,3 +71,9 @@ function validate() {
         }
     }
 }
+
+
+
+
+
+
