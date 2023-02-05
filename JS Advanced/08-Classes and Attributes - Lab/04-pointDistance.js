6983e7123d3e692b83059a8a13e4a35e7c001f55
Maxim Kraychev <theneraxes@gmail.com>
2023-02-05 18:46:07 +0200

    Adding exercise: 04. Point Distance

---
+++ JS Advanced/08-Classes and Attributes - Lab/04-pointDistance.js
@@ -0,0 +1,18 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(p1, p2) {
+        const x = p2.x - p1.x;
+        const y = p2.y - p1.y;
+        return Math.sqrt(x ** 2 + y ** 2);
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
+
+
