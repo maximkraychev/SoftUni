5b0d14f774b17a2bbfb063a9121441b783dcfe92
Maxim Kraychev <theneraxes@gmail.com>
2023-02-13 18:59:06 +0200

    Adding exercise: 02. Library Collection

--- JS Advanced/JS Advanced Exam Practice/JS Advanced Exam Preparation 2/02. Library Collection/libraryCollection.js
+++ JS Advanced/JS Advanced Exam Practice/JS Advanced Exam Preparation 2/02. Library Collection/libraryCollection.js
@@ -1,3 +1,94 @@
 class LibraryCollection {
-// TODO...
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.books = [];
+    }
+
+    addBook(bookName, bookAuthor) {
+        if (this.capacity <= this.books.length) {
+            throw new Error('Not enough space in the collection.');
+        }
+
+        this.books.push({
+            bookName,
+            bookAuthor,
+            payed: false,
+        });
+        return `The ${bookName}, with an author ${bookAuthor}, collect.`
+    }
+
+    payBook(bookName) {
+        const book = this.books.find(x => x.bookName === bookName);
+        if (book === undefined) {
+            throw new Error(`${bookName} is not in the collection.`);
+        }
+        if (book.payed === true) {
+            throw new Error(`${bookName} has already been paid.`);
+        }
+
+        book.payed = true;
+        return `${bookName} has been successfully paid.`;
+    }
+
+    removeBook(bookName) {
+        const bookIndex = this.books.findIndex(x => x.bookName === bookName);
+
+        if (bookIndex === -1) {
+            throw new Error(`The book, you're looking for, is not found.`);
+        }
+
+        if (this.books[bookIndex].payed === false) {
+            throw new Error(`${bookName} need to be paid before removing from the collection.`);
+        }
+        this.books.splice(bookIndex, 1);
+        return `${bookName} remove from the collection.`;
+    }
+
+    getStatistics(bookAuthor) {
+        if (bookAuthor !== undefined) {
+            const book = this.books.find(x => x.bookAuthor === bookAuthor);
+            if (book === undefined) {
+                throw new Error(`${bookAuthor} is not in the collection.`);
+            }
+            return `${book.bookName} == ${book.bookAuthor} - ${book.payed ? 'Has Paid' : 'Not Paid'}.`
+        }
+
+        let stringToReturn = `The book collection has ${this.capacity - this.books.length} empty spots left.`;
+        this.books
+            .sort((a, b) => a.bookName.localeCompare(b.bookName))
+            .forEach(x => stringToReturn += `\n${x.bookName} == ${x.bookAuthor} - ${x.payed ? 'Has Paid' : 'Not Paid'}.`);
+        return stringToReturn
+    }
 }
+
+// const library = new LibraryCollection(2)
+// console.log(library.addBook('In Search of Lost Time', 'Marcel Proust'));
+// console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
+// console.log(library.addBook('Ulysses', 'James Joyce'));
+
+// const library = new LibraryCollection(2)
+// library.addBook('In Search of Lost Time', 'Marcel Proust');
+// console.log(library.payBook('In Search of Lost Time'));
+// console.log(library.payBook('Don Quixote'));
+
+// const library = new LibraryCollection(2)
+// library.addBook('In Search of Lost Time', 'Marcel Proust');
+// library.addBook('Don Quixote', 'Miguel de Cervantes');
+// library.payBook('Don Quixote');
+// console.log(library.removeBook('Don Quixote'));
+// console.log(library.removeBook('In Search of Lost Time'));
+
+// const library = new LibraryCollection(2)
+// console.log(library.addBook('Don Quixote', 'Miguel de Cervantes'));
+// console.log(library.getStatistics('Miguel de Cervantes'));
+
+const library = new LibraryCollection(5)
+library.addBook('Don Quixote', 'Miguel de Cervantes');
+library.payBook('Don Quixote');
+library.addBook('In Search of Lost Time', 'Marcel Proust');
+library.addBook('Ulysses', 'James Joyce');
+console.log(library.getStatistics());
+
+
+
+
