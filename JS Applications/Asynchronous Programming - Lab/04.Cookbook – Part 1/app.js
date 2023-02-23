b4f82bfc2f2bfea45f2270c79d8714dd96cf53e7
Maxim Kraychev <theneraxes@gmail.com>
2023-02-23 12:05:18 +0200

    Adding exercise: 4.Cookbook – Part 1

--- JS Applications/Asynchronous Programming - Lab/04.Cookbook – Part 1/app.js
+++ JS Applications/Asynchronous Programming - Lab/04.Cookbook – Part 1/app.js
@@ -7,7 +7,6 @@ fetch('http://localhost:3030/jsonstore/cookbook/recipes')
 function createPreviewElements(response) {
     Object.values(response)
         .forEach(x => {
-            console.log(x);
             const article = factory('article', false, 'preview');
 
             const divTitle = factory('div', false, 'title', article);
@@ -15,8 +14,50 @@ function createPreviewElements(response) {
 
             const divWithPicture = factory('div', false, 'small', article);
             factory('img', false, false, divWithPicture, x.img);
-            listOfPreviewElements.push({'id': x._id, element: article});
+            listOfPreviewElements.push({ 'id': x._id, element: article });
         })
+}
+
+function info(event) {
+    let articleElement = event.target;
+    while (articleElement.className !== 'preview') {
+        articleElement = articleElement.parentElement;
+    }
+
+    const index = listOfPreviewElements.findIndex(x => x.element == articleElement);
+    if (index == -1) {
+        return;
+    }
+    const id = listOfPreviewElements[index].id;
+    const render = renderInfo.bind(null, articleElement);
+
+    fetch(`http://localhost:3030/jsonstore/cookbook/details/${id}`)
+        .then(response => response.json())
+        .then(render)
+
+}
+
+function renderInfo(article, response) {
+    const allIngredients = response.ingredients.map(x => `<li>${x}</li>`);
+    const allPreparation = response.steps.map(x => `<p>${x}</p>`);
+    article.innerHTML = `
+    <h2>${response.name}</h2>
+            <div class="band">
+                <div class="thumb">
+                    <img src=${response.img}>
+                </div>
+                <div class="ingredients">
+                    <h3>Ingredients:</h3>
+                    <ul>
+                        ${allIngredients.join('\n')}
+                    </ul>
+                </div>
+            </div>
+            <div class="description">
+                <h3>Preparation:</h3>
+                ${allPreparation.join('\n')}
+            </div>
+            `
 }
 
 function factory(el, text, nameClass, appendTo, src) {
@@ -42,6 +83,9 @@ function onLoad() {
     const main = document.querySelector('main');
     main.innerHTML = '';
     listOfPreviewElements.forEach(x => main.appendChild(x.element));
+    main.addEventListener('click', info);
 }
+
+
 
 
