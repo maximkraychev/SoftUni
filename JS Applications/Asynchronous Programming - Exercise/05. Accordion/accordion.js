5beb30c026c46d8de7a6fbb62928ee0560074b67
Maxim Kraychev <theneraxes@gmail.com>
2023-02-26 23:35:39 +0200

    Adding exercise: 05. Accordion; Everything seems to work but only 33/100 in Judge

--- JS Applications/Asynchronous Programming - Exercise/05. Accordion/accordion.js
+++ JS Applications/Asynchronous Programming - Exercise/05. Accordion/accordion.js
@@ -2,57 +2,50 @@ function solution() {
     const mainSection = document.querySelector('#main');
 
     fetch('http://localhost:3030/jsonstore/advanced/articles/list')
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok == false) {
+                throw new Error(`Error: ${respond.statusText} - ${respond.status}`)
+            }
+            return response.json();
+        })
         .then(renderPreview)
         .catch(err => console.log(err))
-
-    mainSection.addEventListener('click', onClick);
-
-    function onClick(event) {
-        const btn = event.target;
-        if (btn.tagName != 'BUTTON') {
-            return
-        }
-
-        const mainDiv = btn.parentElement.parentElement;
-
-        if (btn.textContent == 'Less') {
-            const divToShow = btn.parentElement.nextSibling;
-            divToShow.classList.add('extra');
-            btn.textContent = 'More';
-            return;
-        }
-
-        if (mainDiv.children.length > 1 && btn.textContent == 'More') {
-            const divToHide = btn.parentElement.nextSibling;
-            divToHide.classList.remove('extra');
-            btn.textContent = 'Less';
-            return;
-        }
-
-        const id = btn.id;
-
-        fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${id}`)
-            .then(response => response.json())
-            .then(moreInfo)
-            .catch(err => console.log(err))
-
-        function moreInfo(data) {
-            const moreInfoDiv = factory('div', false);
-            factory('p', data.content, false, false, moreInfoDiv);
-            mainDiv.appendChild(moreInfoDiv);
-            btn.textContent = 'Less';
-        }
-    }
-
 
     function renderPreview(data) {
         data.forEach(x => {
             const mainDiv = factory('div', false, 'accordion');
             const headDiv = factory('div', false, 'head', false, mainDiv);
             factory('span', x.title, false, false, headDiv);
-            factory('button', 'More', 'button', x._id, headDiv);
+            const button = factory('button', 'More', 'button', x._id, headDiv);
+            const extraDiv = factory('div', false, 'extra', false, mainDiv);
             mainSection.appendChild(mainDiv);
+            button.addEventListener('click', onClick);
+
+            function onClick() {
+                if (extraDiv.querySelector('p') == null) {
+                    fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${x._id}`)
+                        .then(response => {
+                            if (response.ok == false) {
+                                throw new Error(`Error: ${respond.statusText} - ${respond.status}`)
+                            }
+                            return response.json();
+                        })
+                        .then(moreInfo)
+                        .catch(err => console.log(err))
+
+                    function moreInfo(data) {
+                        factory('p', data.content, false, false, extraDiv);
+                    }
+                }
+
+                if (button.textContent == 'Less') {
+                    extraDiv.classList.add('extra');
+                    button.textContent = 'More';
+                } else if (button.textContent == 'More') {
+                    extraDiv.classList.remove('extra');
+                    button.textContent = 'Less';
+                }
+            }
         })
     }
 
