77f750fe391f8cef18099b0bfbeae194a6b1b992
Maxim Kraychev <theneraxes@gmail.com>
2023-03-11 10:40:14 +0200

    Adding exercise: 01. Accordion

--- JS Applications/Architecture and Testing - Lab/01. Accordion/test.js
+++ JS Applications/Architecture and Testing - Lab/01. Accordion/test.js
@@ -1,20 +1,12 @@
 const { chromium } = require('playwright-chromium');
 const { expect } = require('chai');
 
-// async () => {
-//     const browser = await chromium.launch();
-//     const page = await browser.newPage();
-//     await page.goto('http://google.com');
-//     await browser.close();
-// };
-
-
 let browser, page;
 
 describe('E2E tests', async function () {
     this.timeout(6000);
 
-    before(async () => { browser = await chromium.launch(); });
+    before(async () => { browser = await chromium.launch(/*{ headless: false, slowMo: 500 }*/); });
     after(async () => { await browser.close(); });
     beforeEach(async () => { page = await browser.newPage(); });
     afterEach(async () => { await page.close(); });
@@ -22,7 +14,6 @@ describe('E2E tests', async function () {
 
     it('load static page', async () => {
         await page.goto('http://127.0.0.1:5500/01.%20Accordion/');
-        //await page.screenshot({path: 'test.png'});
         const titles = await page.locator('div.head>span').allTextContents();
 
         expect(titles[0]).to.equal('Scalable Vector Graphics');
@@ -33,13 +24,29 @@ describe('E2E tests', async function () {
 
     it('button “More” functionality', async () => {
         await page.goto('http://127.0.0.1:5500/01.%20Accordion/');
-        
+
         await page.getByText('More').first().click();
-        
+
+
         const visible = await page.isVisible('div.extra');
         expect(visible).to.be.true;
-        
+
         const content = await page.locator('div.extra p').first().allTextContents();
         expect(content.length).to.be.greaterThan(0);
+
+        const visibleBtn = await page.getByText('Less').isVisible()
+        expect(visibleBtn).to.be.true;
+    });
+
+    it('button “Less” functionality', async () => {
+        await page.goto('http://127.0.0.1:5500/01.%20Accordion/');
+
+        await page.getByText('More').first().click();
+        const visible = await page.isVisible('div.extra');
+        expect(visible).to.be.true;
+
+        await page.getByText('Less').click();
+        const visibleAfter = await page.isVisible('div.extra');
+        expect(visibleAfter).to.be.false;
     });
 });
