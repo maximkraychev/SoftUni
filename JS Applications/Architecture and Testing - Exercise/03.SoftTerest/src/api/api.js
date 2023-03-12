653779d53a15d8c01f76aa68f187e412981e3dec
Maxim Kraychev <theneraxes@gmail.com>
2023-03-12 12:38:24 +0200

    Adding module that makes the requests to the REST server.

---
+++ JS Applications/Architecture and Testing - Exercise/03.SoftTerest/src/api/api.js
@@ -0,0 +1,54 @@
+const host = 'http://localhost:3030';
+
+async function request(type, url, data) {
+    const options = {
+        method: type,
+        headers: {}
+    }
+
+    if (data !== undefined) {
+        options.headers['Content-type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+        const token = user.accessToken;
+        options.headers['X-Authorization'] = token;
+    }
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if (response.ok == false) {
+            if (response.status == 403) {
+                localStorage.removeItem('user');
+            }
+
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status == 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
+    }
+}
+
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+export {
+    get,
+    post,
+    put,
+    del as delete
+}
