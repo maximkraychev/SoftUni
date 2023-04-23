a9f1ad47a9a1d50e4f8979aae73c02170078da9e
Maxim Kraychev <theneraxes@gmail.com>
2023-04-23 12:25:57 +0300

    Fixing the issue where the validation of Cube schema doesn't work correctly. (It turns out it was because of wrong syntax...)

--- JS Back-End/MongoDB and Mongoose/04. Cubicle-Workshop-Part-2-Resources/models/Accessor.js
+++ JS Back-End/MongoDB and Mongoose/04. Cubicle-Workshop-Part-2-Resources/models/Accessor.js
@@ -1,17 +1,17 @@
-const {Schema, model, Types} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const accessorSchema = new Schema({
-    name: {type: String, require: true},
+    name: { type: String, require: true },
     imageUrl: {
-        type: String, 
-        require: true, 
-        validator: function(value) {
-            const regex = /^(?:https?|http)/g;
-            return regex.test(value);
-        },
-        message: props => `${props.value} is not a valid URL` },
-    description: {type: String, require: true, maxlength: 20},
-    cubes: {type: [Types.ObjectId], default: [], ref: 'Cube'}
+        type: String,
+        require: true,
+        validate: {
+            validator: (value) => /^(?:https?|http)/g.test(value),
+            message: props => `${props.value} is not a valid URL`
+        }
+    },
+    description: { type: String, require: true, maxlength: 20 },
+    cubes: { type: [Types.ObjectId], default: [], ref: 'Cube' }
 });
 
 const Accessor = model('Accessor', accessorSchema);
--- JS Back-End/MongoDB and Mongoose/04. Cubicle-Workshop-Part-2-Resources/models/Cube.js
+++ JS Back-End/MongoDB and Mongoose/04. Cubicle-Workshop-Part-2-Resources/models/Cube.js
@@ -6,13 +6,21 @@ const cubeSchema = new Schema({
     imageUrl: {
         type: String,
         require: true,
-        validator: (value) => /^(?:https?|http)/g.test(value),
-        message: props => `${props.value} is not a valid URL`
+        validate: {
+            validator: (value) => /^(?:https?|http)/g.test(value),
+            message: props => `${props.value} is not a valid URL`
+        }
     },
     difficultyLevel: { type: Number, require: true, min: [1, 'Minimum difficalty level is 1'], max: [6, 'Maximum difficalty leel is 6'] },
-    accessories: {type: [Types.ObjectId], default: [], ref: 'Accessor'}
+    accessories: { type: [Types.ObjectId], default: [], ref: 'Accessor' }
 });
 
 const Cube = model('Cube', cubeSchema);
 
 module.exports = Cube;
+
+// Validation can be done like this as well;
+
+// cubeSchema.path('imageUrl').validate(function () {
+//     return this.imageUrl.startsWith('http');
+// }, 'Image should be a link!');
