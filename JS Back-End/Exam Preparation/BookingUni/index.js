a9fff81cd2f993159bde5acffc792f908c327a5a
Maxim Kraychev <theneraxes@gmail.com>
2023-05-04 23:41:58 +0300

    Add database setup.

--- JS Back-End/Exam Preparation/BookingUni/index.js
+++ JS Back-End/Exam Preparation/BookingUni/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const expressConfig = require('./config/express');
 const routesConfig = require('./config/routes');
+const databaseConfig = require('./config/database');
 
 const port = 3000;
 
 async function start() {
     const app = express();
 
+    await databaseConfig(app);
     expressConfig(app);
     routesConfig(app);
 
