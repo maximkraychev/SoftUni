92b12193a7e40afcd2e9ccb0a7c20edf807a71e4
Maxim Kraychev <theneraxes@gmail.com>
2023-05-10 11:39:46 +0300

    Add user and guest view and logic for home page.

--- JS Back-End/Exam Preparation/Tutorials/controllers/homeController.js
+++ JS Back-End/Exam Preparation/Tutorials/controllers/homeController.js
@@ -1,15 +1,29 @@
+const { getAllCoursesByDate, getThreeCoursesByEnrolledUsers } = require('../services/course');
+const { parseError } = require('../utils/parsers');
+
 const homeController = require('express').Router();
 
-homeController.get('/', (req, res) => {
-    let template;
+homeController.get('/', async (req, res) => {
+    const template = req.user ? 'userHome' : 'guestHome';
+    let courses;
 
-    if (req.user) {
-        template = 'userHome';
-    } else {
-        template = 'guestHome';
+    try {
+        if (template == 'userHome') {
+            courses = await getAllCoursesByDate();
+        } else {
+            courses = await getThreeCoursesByEnrolledUsers();
+            courses.forEach(x => x.usersEnrolled = x.usersEnrolled.length);
+        }
+
+        res.render(template, {
+            courses
+        });
+    } catch (err) {
+        res.render(template, {
+            error: parseError(err)
+        });
     }
 
-    res.render(template);
 });
 
 module.exports = homeController;
--- JS Back-End/Exam Preparation/Tutorials/services/course.js
+++ JS Back-End/Exam Preparation/Tutorials/services/course.js
@@ -2,7 +2,6 @@ const Course = require("../models/course");
 const { dateParser } = require("../utils/parsers");
 
 async function createCourse({ title, description, imageUrl, duration }) {
-
     const existing = await Course.findOne({ title }).collation({ locale: 'en', strength: 2 });
 
     if (existing) {
@@ -10,14 +9,27 @@ async function createCourse({ title, description, imageUrl, duration }) {
     }
 
     return Course.create({
-            title,
-            description,
-            imageUrl,
-            duration,
-            createdAt: dateParser() 
+        title,
+        description,
+        imageUrl,
+        duration,
+        createdAt: dateParser()
     });
+}
+
+async function getAllCoursesByDate() {
+    return Course.find({}).sort({ createdAt: 1 }).lean();
+}
+
+async function getThreeCoursesByEnrolledUsers() {
+    return Course.find({})
+        .sort({ 'usersEnrolled.length': -1 })
+        .limit(3)
+        .lean()
 }
 
 module.exports = {
     createCourse,
+    getAllCoursesByDate,
+    getThreeCoursesByEnrolledUsers
 }
