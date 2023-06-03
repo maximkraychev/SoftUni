dfdec917ffbd5a6da1126a92bc9c7fd0ad118741
Maxim Kraychev <theneraxes@gmail.com>
2023-06-03 14:55:53 +0300

    Add ArticleComponent properties and methods

--- Angular/02-Components/src/app/article/article.component.ts
+++ Angular/02-Components/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,4 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent {
+  private symbols: number = 250;
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = 'Show Image';
+
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
+
+  readMore(): void {
+    this.articleDescLen += this.symbols;
+
+    if (this.articleDescLen > this.articleDesc.length) {
+      this.showReadMoreBtn = false;
+      this.showHideBtn = true;
+    } else {
+
+      this.descToShow = this.articleDesc.substring(0, this.articleDescLen);
+    }
+  }
+
+  ToggleImage(): void {
+    this.imageIsShown = !this.imageIsShown;
+    this.imageButtonTitle = this.imageButtonTitle == 'Show Image' ? 'Hide Image' : 'Show Image';
+  }
+
+  HideDesc(): void {
+    this.descToShow = '';
+    this.articleDescLen = 0;
+    this.showHideBtn = false;
+    this.showReadMoreBtn = true;
+  }
 }
