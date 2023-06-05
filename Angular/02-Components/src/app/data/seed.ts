90fa1571aa12f2c849a2e87cc31ed35d681b6305
Maxim Kraychev <theneraxes@gmail.com>
2023-06-05 12:35:50 +0300

    add longer descriptions

--- Angular/02-Components/src/app/data/seed.ts
+++ Angular/02-Components/src/app/data/seed.ts
@@ -1,18 +1,18 @@
 const data = [{
     title: 'Article 1',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis neque voluptatum, quae nam voluptatem id nobis deleniti veniam error optio corrupti, explicabo delectus esse hic sequi reiciendis deserunt eos recusandae. Suscipit mollitia quod vero atque tempora totam nesciunt, sunt asperiores facilis dolore quia, officiis commodi impedit nulla corporis possimus.',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur molestiae quod ab quo ipsa natus eum exercitationem! Illo exercitationem adipisci recusandae! Nam quaerat maiores animi sequi a eveniet tempora illum, quia deserunt explicabo velit. Aliquid ullam animi saepe cupiditate, aperiam ea, iste ex suscipit ipsum laborum laudantium quia soluta. Optio doloremque magnam architecto, ullam nulla consequuntur aliquam? Repellendus, maxime. Magni itaque aut architecto. Adipisci provident pariatur tempore eos ipsa quam dolor ducimus accusantium dolores velit tempora officiis, recusandae nostrum cum consequuntur! Deserunt, modi odio quia a unde maxime, obcaecati minus in quibusdam atque eius sunt voluptatum? Ab ipsa ipsam minima.',
     author: 'Author 1',
     imageUrl: 'http://ichef.bbci.co.uk/news/976/cpsprodpb/10434/production/_90121666_agreementcartoon.jpg'
 },
 {
     title: 'Article 2',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis neque voluptatum, quae nam voluptatem id nobis deleniti veniam error optio corrupti, explicabo delectus esse hic sequi reiciendis deserunt eos recusandae. Suscipit mollitia quod vero atque tempora totam nesciunt, sunt asperiores facilis dolore quia, officiis commodi impedit nulla corporis possimus.',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur molestiae quod ab quo ipsa natus eum exercitationem! Illo exercitationem adipisci recusandae! Nam quaerat maiores animi sequi a eveniet tempora illum, quia deserunt explicabo velit. Aliquid ullam animi saepe cupiditate, aperiam ea, iste ex suscipit ipsum laborum laudantium quia soluta. Optio doloremque magnam architecto, ullam nulla consequuntur aliquam? Repellendus, maxime. Magni itaque aut architecto. Adipisci provident pariatur tempore eos ipsa quam dolor ducimus accusantium dolores velit tempora officiis, recusandae nostrum cum consequuntur! Deserunt, modi odio quia a unde maxime, obcaecati minus in quibusdam atque eius sunt voluptatum? Ab ipsa ipsam minima.',
     author: 'Author 2',
     imageUrl: 'http://www.digitalmeetsculture.net/wp-content/uploads/2015/04/article.jpg'
 },
 {
     title: 'Article 3',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis neque voluptatum, quae nam voluptatem id nobis deleniti veniam error optio corrupti, explicabo delectus esse hic sequi reiciendis deserunt eos recusandae. Suscipit mollitia quod vero atque tempora totam nesciunt, sunt asperiores facilis dolore quia, officiis commodi impedit nulla corporis possimus.',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur molestiae quod ab quo ipsa natus eum exercitationem! Illo exercitationem adipisci recusandae! Nam quaerat maiores animi sequi a eveniet tempora illum, quia deserunt explicabo velit. Aliquid ullam animi saepe cupiditate, aperiam ea, iste ex suscipit ipsum laborum laudantium quia soluta. Optio doloremque magnam architecto, ullam nulla consequuntur aliquam? Repellendus, maxime. Magni itaque aut architecto. Adipisci provident pariatur tempore eos ipsa quam dolor ducimus accusantium dolores velit tempora officiis, recusandae nostrum cum consequuntur! Deserunt, modi odio quia a unde maxime, obcaecati minus in quibusdam atque eius sunt voluptatum? Ab ipsa ipsam minima.',
     author: 'Author 3',
     imageUrl: 'http://i.dailymail.co.uk/i/pix/2010/04/17/article-1266852-092DE58A000005DC-112_634x411.jpg'
 },
