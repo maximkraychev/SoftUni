369846e77f31fa062d312d325e63eaac83d59b42
Maxim Kraychev <theneraxes@gmail.com>
2023-06-12 16:43:28 +0300

    add ads schema

--- JS Back-End/Exam Preparation/Job-Ads/models/product.js
+++ JS Back-End/Exam Preparation/Job-Ads/models/product.js
@@ -1,8 +1,21 @@
 const { Schema, model, Types } = require('mongoose');
 
 const productSchema = new Schema({
-    //TODO... requirements 
+    headline: {type: String, required: true}, 
+    location: {type: String, required: true}, 
+    companyName: {type: String, required: true}, 
+    companyDescription : {type: String, required: true}, 
+    author: {type: Types.ObjectId, ref: 'User', required: true}, 
+    usersApplied: {type: [Types.ObjectId], ref: 'User', default: []}, 
 });
+
+//TODO chek if it works with ObjectId
+productSchema.index({author: 1}, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+})
 
 const Product = model('Product', productSchema);
 
