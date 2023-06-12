d97e83eaa00a5606a7a98bb6d871c5b75133dedd
Maxim Kraychev <theneraxes@gmail.com>
2023-06-12 16:12:07 +0300

    Add scaffold

---
+++ JS Back-End/Exam Preparation/Job-Ads/controllers/catalogController.js
@@ -0,0 +1,26 @@
+const catalogCotroller = require('express').Router();
+const { getAllProducts } = require('../services/product');
+const parseError = require('../utils/parsers');
+
+//TODO... Chnage: (Path), (Name of the Template), (Title)
+catalogCotroller.get('/', async (req, res) => {
+
+    try {
+        const products = await getAllProducts();
+        res.render('catalog', {
+            title: '',
+            products,
+            user: req.user
+        });
+    } catch (err) {
+        //TODO Some logic for error case!!!
+        console.error(err);
+        // res.render('catalog', {
+        //     title: 'Catalog Page - Gaming Team',
+        //     error: parseError({ message: 'Something went wrong please try again later!' })
+        // })
+    }
+
+});
+
+module.exports = catalogCotroller;
---
+++ JS Back-End/Exam Preparation/Job-Ads/models/user.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    //TODO... Add or remove properties
+    username: { type: String, required: true, unique: true, minLength: [5, 'The username should be at least five characters long'] },
+    email: { type: String, required: true, unique: true, minLength: [10, 'The email should be at least ten character long'] },
+    hashedPassword: { type: String, required: true },
+});
+
+//TODO... add ore removed indexs
+userSchema.index({ username: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+
+userSchema.index({ email: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
