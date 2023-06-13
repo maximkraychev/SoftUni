8d76447cdba7d65db1f81d26bd360b9b7269e07c
Maxim Kraychev <theneraxes@gmail.com>
2023-06-13 16:21:30 +0300

    init

---
+++ JS Back-End/Exam Preparation/Petstagram/services/product.js
@@ -0,0 +1,47 @@
+const Product = require("../models/product");
+
+//TODO... Chnage the properties for destructuring;
+async function createProduct({ name }, owner) {
+
+    return Product.create({
+        name,
+        owner
+    });
+}
+
+async function getAllProducts() {
+    return Product.find({}).lean();
+}
+
+async function getProductRow(id) {
+    return Product.findById(id);
+}
+
+async function getProduct(id) {
+    return Product.findById(id).lean();
+}
+
+async function deleteProduct(id) {
+    return Product.findByIdAndDelete(id);
+}
+
+//TODO... Chnage the properties for destructuring
+//TODO... Chnage the search
+async function findProductBySearch({ search, platform }) {
+    const games = await Product
+        .find({
+            name: { $regex: search, $options: 'i' },
+            platform: { $regex: platform, $options: 'i' }
+        })
+        .lean();
+    return games;
+}
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+    getProductRow,
+    getProduct,
+    deleteProduct,
+    findProductBySearch
+}
