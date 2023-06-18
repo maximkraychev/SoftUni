4050680f2ce1e7228aba0aeaf0d37826469c6d24
Maxim Kraychev <theneraxes@gmail.com>
2023-06-18 14:24:30 +0300

    Add register login logout

--- JS Back-End/Exam/controllers/authController.js
+++ JS Back-End/Exam/controllers/authController.js
@@ -3,49 +3,43 @@ const { isUser, isGuest } = require('../middlewares/guards');
 const { register, login } = require('../services/user');
 const parseError = require('../utils/parsers');
 
-//TODO... Add title and name of template
 authController.get('/register', isGuest(),  (req, res) => {
-    res.render('register', { title: '' });
+    res.render('register', { title: 'Register Page' });
 });
 
-//TODO... chnage the name of the template and redirect 
 authController.post('/register', isGuest(), async (req, res) => {
     try {
         const token = await register(req.body);
         res.cookie('token', token, { httpOnly: true });
         res.redirect('/');
     } catch (err) {
-        //TODO.. Chnage the title and name of template
         res.render('register', {
-            title: '',
+            title: 'Register Page',
             body: req.body,
             error: parseError(err)
         })
     }
 });
 
-//TODO... chnage the title and name of the template
 authController.get('/login', isGuest(), (req, res) => {
-    res.render('login', { title: '' });
+    res.render('login', { title: 'Login Page' });
 });
 
-//TODO... chnage the name of the template and redirect
+
 authController.post('/login', isGuest(), async (req, res) => {
     try {
         const token = await login(req.body);
         res.cookie('token', token, { httpOnly: true });
         res.redirect('/');
     } catch (err) {
-        //TODO... chnage the title and name of the template
         res.render('login', {
-            title: '',
+            title: 'Login Page',
             body: req.body,
             error: parseError(err)
         });
     }
 });
 
-//TODO... chnage the name of the template and redirect 
 authController.get('/logout', isUser(), (req, res) => {
     res.clearCookie('token');
     res.redirect('/');
