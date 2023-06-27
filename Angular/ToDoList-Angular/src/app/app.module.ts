0283527415f08e2fd0a69b4071bbc03734831ac1
Maxim Kraychev <theneraxes@gmail.com>
2023-06-27 12:06:35 +0300

    add structure of main emty inner comppnents

--- Angular/ToDoList-Angular/src/app/app.module.ts
+++ Angular/ToDoList-Angular/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './main/header/header.component';
+import { TodolistComponent } from './main/todolist/todolist.component';
+import { ListItemComponent } from './main/todolist/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodolistComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular/ToDoList-Angular/src/app/main/todolist/todolist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent {
+
+}
