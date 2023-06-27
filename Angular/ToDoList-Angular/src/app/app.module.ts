baae45302da48b0273d583bf5a3ebc69fec8b2e2
Maxim Kraychev <theneraxes@gmail.com>
2023-06-27 12:02:16 +0300

    Add main component

--- Angular/ToDoList-Angular/src/app/app.module.ts
+++ Angular/ToDoList-Angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
