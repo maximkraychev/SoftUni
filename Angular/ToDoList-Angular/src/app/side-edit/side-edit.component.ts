cf6268ba364ba73d5a55e671cc7f346e95e9e1c8
Maxim Kraychev <theneraxes@gmail.com>
2023-06-29 17:57:58 +0300

    implement error service to side-edit component

--- Angular/ToDoList-Angular/src/app/side-edit/side-edit.component.ts
+++ Angular/ToDoList-Angular/src/app/side-edit/side-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../services/api-data.service';
 import { EditService } from '../services/edit.service';
+import { ErrorService } from '../services/error.service';
 
 @Component({
   selector: 'app-side-edit',
@@ -11,13 +12,14 @@ export class SideEditComponent {
 
   constructor(
     private apiService: ApiService,
+    private errorService: ErrorService,
     public editService: EditService
   ) { }
 
   edit(title: string): void {
 
-    if(title == '') {
-      return console.log('The task title cannot be empty string!');
+    if (title == '') {
+      return this.errorService.setError('The task title cannot be empty string!');
     }
 
     this.apiService.editTask(this.editService.taskToEdit, { title, completed: false });
