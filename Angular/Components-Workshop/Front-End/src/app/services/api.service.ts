5931a145bff7a0003aea5f86add19d8421a4e0a0
Maxim Kraychev <theneraxes@gmail.com>
2023-07-04 14:52:49 +0300

    moved api service to services folder

--- Angular/Components-Workshop/Front-End/src/app/services/api.service.ts
+++ Angular/Components-Workshop/Front-End/src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ITheme } from './interfaces/theme';
-import { IPost } from './interfaces/post';
+import { ITheme } from '../interfaces/theme';
+import { IPost } from '../interfaces/post';
 
 @Injectable({
   providedIn: 'root'
