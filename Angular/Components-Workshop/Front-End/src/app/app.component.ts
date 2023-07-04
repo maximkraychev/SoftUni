4861ff8b26420d2b849227cb37c68e1c896c921d
Maxim Kraychev <theneraxes@gmail.com>
2023-07-04 13:10:55 +0300

    change the title of the angular app

--- Angular/Components-Workshop/Front-End/src/app/app.component.ts
+++ Angular/Components-Workshop/Front-End/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Components-Workshop-Part-1';
+  title = 'Components-Workshop';
 }
