f688520b2d8d3b99453a3b5ee5cc881ef5df025a
Maxim Kraychev <theneraxes@gmail.com>
2023-07-04 13:32:24 +0300

    add spinner component

--- Angular/Components-Workshop/Front-End/src/app/core/core.module.ts
+++ Angular/Components-Workshop/Front-End/src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SpinnerComponent
   ]
 })
 export class CoreModule { }
