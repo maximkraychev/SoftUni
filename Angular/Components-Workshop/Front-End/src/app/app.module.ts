319f7605ef2f6088dfe6b1cee19b92dc785349e8
Maxim Kraychev <theneraxes@gmail.com>
2023-07-04 22:34:38 +0300

    moved theme-list and recen-post to core module

--- Angular/Components-Workshop/Front-End/src/app/app.module.ts
+++ Angular/Components-Workshop/Front-End/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { CoreModule } from './core/core.module'; './core/core.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ThemeListComponent } from './theme-list/theme-list.component';
-import { RecentPostListComponent } from './recent-post-list/recent-post-list.component';
+import { ThemeListComponent } from './core/theme-list/theme-list.component';
+import { RecentPostListComponent } from './core/recent-post-list/recent-post-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
 import { SharedModule } from './shared/shared.module';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [
@@ -20,9 +21,10 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
     HttpClientModule,
-    SharedModule
+    CoreModule,
+    SharedModule,
+    FeaturesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/Components-Workshop/Front-End/src/app/core/recent-post-list/recent-post-list.component.ts
+++ Angular/Components-Workshop/Front-End/src/app/core/recent-post-list/recent-post-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../services/api.service';
-import { IPost } from '../interfaces/post';
+import { ApiService } from '../../services/api.service';
+import { IPost } from '../../interfaces/post';
 
 @Component({
   selector: 'app-recent-post-list',
