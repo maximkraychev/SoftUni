735c829fa6a15011994924f909f13ab045080fa7
Maxim Kraychev <theneraxes@gmail.com>
2023-07-04 22:50:20 +0300

    add home component

--- Angular/Components-Workshop/Front-End/src/app/features/features.module.ts
+++ Angular/Components-Workshop/Front-End/src/app/features/features.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class FeaturesModule { }
