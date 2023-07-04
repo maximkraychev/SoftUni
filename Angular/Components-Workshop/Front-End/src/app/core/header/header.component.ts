1f403a9df8c239c2f9c27b692aea0ab7cdd3591e
Maxim Kraychev <theneraxes@gmail.com>
2023-07-04 14:53:57 +0300

    add navigation

--- Angular/Components-Workshop/Front-End/src/app/core/header/header.component.ts
+++ Angular/Components-Workshop/Front-End/src/app/core/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MockAuthService } from 'src/app/services/mock-auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isLogin: boolean = false;
+  private authSub!: Subscription;
 
+  constructor(private auth: MockAuthService) {}
+
+  login() {
+    this.auth.login();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+  ngOnInit(): void {
+    this.authSub = this.auth.authObservable.subscribe((data) => {
+      this.isLogin = data;
+    }, (err) => {
+      console.log(err);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
 }
--- Angular/Components-Workshop/Front-End/src/app/recent-post-list/recent-post-list.component.ts
+++ Angular/Components-Workshop/Front-End/src/app/recent-post-list/recent-post-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { ApiService } from '../services/api.service';
 import { IPost } from '../interfaces/post';
 
 @Component({
