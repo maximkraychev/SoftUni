c3e39d8c7d0e5084c15227c52f98551754b3a412
MaximKraychev <maxim.kraychev@gmail.com>
2024-02-08 23:10:06 +0200

    Add class DepositCalculator

--- Programming Basics Java/src/FirstStepsInCodingExercise/DepositCalculator.java
+++ Programming Basics Java/src/FirstStepsInCodingExercise/DepositCalculator.java
@@ -1,2 +1,17 @@
-package FirstStepsInCodingExercise;public class DepositCalculator {
+package FirstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class DepositCalculator {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        double sum = Double.parseDouble(scan.nextLine());
+        int depositMonths = Integer.parseInt(scan.nextLine());
+        double annualInterestRate = Double.parseDouble(scan.nextLine());
+
+        double totalInterest = sum * (annualInterestRate / 100);
+        double interestPerMonth = totalInterest / 12;
+
+        System.out.println(sum + depositMonths * interestPerMonth);
+    }
 }
