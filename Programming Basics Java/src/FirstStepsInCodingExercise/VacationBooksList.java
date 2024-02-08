6615c67765f2f4e1ba99a0c9ecacd1bd0a8eef34
MaximKraychev <maxim.kraychev@gmail.com>
2024-02-09 00:02:00 +0200

    Add class VacationBooksList

--- Programming Basics Java/src/FirstStepsInCodingExercise/VacationBooksList.java
+++ Programming Basics Java/src/FirstStepsInCodingExercise/VacationBooksList.java
@@ -1,2 +1,17 @@
-package FirstStepsInCodingExercise;public class VacationBooksList {
+package FirstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class VacationBooksList {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+
+        int totalNumberOfPages = Integer.parseInt(scan.nextLine());
+        int pagesPerHour = Integer.parseInt(scan.nextLine());
+        int days = Integer.parseInt((scan.nextLine()));
+
+        int hoursToReadTheBook = totalNumberOfPages / pagesPerHour;
+
+        System.out.println(hoursToReadTheBook / days);
+    }
 }
