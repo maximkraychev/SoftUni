182dd0ebacfaea5ff99b8799302ec12ccfb1f4fa
MaximKraychev <maxim.kraychev@gmail.com>
2024-02-08 22:50:25 +0200

    Add class RadiansToDegrees

--- Programming Basics Java/src/FirstStepsInCodingExercise/RadiansToDegrees.java
+++ Programming Basics Java/src/FirstStepsInCodingExercise/RadiansToDegrees.java
@@ -1,2 +1,11 @@
-package FirstStepsInCodingExercise;public class RadiansToDegrees {
+package FirstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class RadiansToDegrees {
+    public static void main(String[] arg) {
+        Scanner scan = new Scanner(System.in);
+        double radians = Double.parseDouble(scan.nextLine());
+        System.out.println((radians * 180) / Math.PI);
+    }
 }
