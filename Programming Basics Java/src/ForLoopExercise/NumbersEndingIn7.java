fd5d2bf396a910bdeb571884929a89c66c0749a0
MaximKraychev <maxim.kraychev@gmail.com>
2024-02-15 16:08:09 +0200

    Add exercise 01. Numbers Ending in 7

---
+++ Programming Basics Java/src/ForLoopExercise/NumbersEndingIn7.java
@@ -0,0 +1,11 @@
+ package ForLoopExercise;
+
+public class NumbersEndingIn7 {
+    public static void main(String[] args) {
+        for (int i = 7; i <= 997; i++) {
+            if(i % 10 == 7) {
+                System.out.println(i);
+            }
+        }
+    }
+}
